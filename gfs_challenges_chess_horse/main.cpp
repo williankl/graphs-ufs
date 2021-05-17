d9aa6206fd616865145fc72d81c9c604c7e18557
Willian Mota <wmoliveira1000@gmail.com>
2021-05-17 09:25:22 -0300

    printing info on events and added output sent

--- gfs_challenges_chess_horse/main.cpp
+++ gfs_challenges_chess_horse/main.cpp
@@ -9,11 +9,20 @@ int main(int filesCount, char** file){
 
     ChessHorseHandler handler;
 
+    std::cout << "Building graph to represent the horse movements...\n";
     Graph g = handler.getHorsePath();
 
-    printLines(getNodesInfo(g.getNodes()));
-    printLines(getPathInfo(g.findHamiltonianPath()));
+    std::string outputName = "output";
+    std::vector<std::string> results;
 
+    results.push_back("Created nodes:\n");
+    results = joinVectors(results, getNodesInfo(g.getNodes()));
 
+    std::cout << "Finding Hamiltonian path...\n";
+    results.push_back("Hamiltonian path:\n");
+    results = joinVectors(results, getPathInfo(g.findHamiltonianPath()));
+
+    makeFile(outputName, results);
+    std::cout << "Saved result at " << outputName << ".txt file.\n\n";
     return 1;
 }
